9d6125966fd38016fb176112d7628e2952dd96c6
ak04-02 <ak04_02@outlook.com>
2025-03-06 09:04:07 -0500

    commit from lab

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -13,21 +13,11 @@ async function connectToDatabase() {
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
-    try {
-        // Task 1: Connect to MongoDB
-        await client.connect();
-
-        // Task 2: Connect to database giftDB and store in variable dbInstance
-        const dbInstance = client.db(dbName);
-
-        // Task 3: Return the database instance
-        return dbInstance;
-    } catch (err) {
-        console.error(err);
-        // Handle connection error
-    }
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
