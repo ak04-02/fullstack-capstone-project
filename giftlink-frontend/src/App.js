4216201b2aaca6bcb53c2af22e44cf863d7cef6a
ak04-02 <ak04_02@outlook.com>
2025-03-06 23:37:53 -0500

    commit from lab

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,6 +6,7 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Profile from './components/Profile/Profile';
 import SearchPage from './components/SearchPage/SearchPage';
 import Navbar from './components/Navbar/Navbar';
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/search" element={<SearchPage/>} />
           <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/profile" element={<Profile/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
